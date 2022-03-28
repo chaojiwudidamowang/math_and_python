88b2dfb69533f9e985d3b8a309ca592d005cd8df
chaojiwudidamowang <z17691381015@163.com>
2022-03-28 19:39:08 +0800

    master

--- mathday1.py
+++ mathday1.py
@@ -1,7 +1,6 @@
-#1
-import os
 import sympy
-print("hello")
+from sympy import oo
+#使用python求lim(x->1) (x^2-1)/(x-1)
 x=sympy.symbols('x')
-f=(x/sympy.sqrt(x*x+x))**x
-print(sympy.limit(f,x,sympy.oo))
+f=(x**2-1)/(x-1)
+print(sympy.limit(f,x,1))
