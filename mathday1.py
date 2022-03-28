ea314a046d330e89c48fcefab927a72d26b4c9fa
chaojiwudidamowang <z17691381015@163.com>
2022-03-28 20:35:38 +0800

    master

--- mathday1.py
+++ mathday1.py
@@ -1,6 +1,20 @@
+from cmath import sin
+import imp
 import sympy
+import numpy as np
 from sympy import oo
+#from sympy import sin
+
 #使用python求lim(x->1) (x^2-1)/(x-1)
 x=sympy.symbols('x')
 f=(x**2-1)/(x-1)
 print(sympy.limit(f,x,1))
+
+#求lim(x->0) sinx/(3x+x^3)
+f=sympy.sin(x)/(3*x+x**3)
+print(sympy.limit(f,x,0))  
+
+#求y=arcsin根号下sinx的导数
+from sympy import *
+from sympy.abc import x,y,z,f
+print(diff(asin(sqrt(sympy.sin(x)))))
